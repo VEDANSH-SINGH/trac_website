268f6c0abf1442ea94eb70e17e71841f54a621c1
VEDANSH-SINGH <66863627+VEDANSH-SINGH@users.noreply.github.com>
2020-11-30 20:11:11 +0530

    added files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+var path=require("path");
+app=express();
+
+app.get("/",function(req,res){
+ res.sendFile(__dirname+"/index.html");
+});
+app.use(express.static(__dirname + '/assets'));
+app.listen(3000,function(){
+    console.log("server is running at port 3000");
+});
